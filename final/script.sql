abbf0286a0723aa2e3241788c7023c19eaf67ccb
Adhish <99695844+apaudel-umd@users.noreply.github.com>
2023-08-16 12:50:39 -0400

    completed the pipeline for form to SQL

--- final/script.sql
+++ final/script.sql
@@ -7,5 +7,19 @@ CREATE TABLE polygon_geom_data (
     geom geometry
 );
 
+DROP TABLE IF EXISTS form_data;
+CREATE TABLE form_data (
+    form_id SERIAL PRIMARY KEY,
+    loc_id INT REFERENCES polygon_geom_data(id),
+    name VARCHAR(100), 
+    email VARCHAR(250), 
+    item VARCHAR(250),
+    color VARCHAR(100),
+    date DATE, 
+    reason VARCHAR(500),
+    extra VARCHAR(250)
+);
+
+
 CREATE INDEX spatial_data_geom_idx ON polygon_geom_data USING GIST (geom);
 
